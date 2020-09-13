b038f7cc606c5cc5c9415a84b3cb672fad844a70
JunShern <chanjunshern@gmail.com>
2020-09-13 16:33:17 +0800

    Simple agent for Taxi-v3 with Expected Sarsa. Best average reward: 8.8

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -11,7 +11,14 @@ class Agent:
         - nA: number of actions available to the agent
         """
         self.nA = nA
+        self.actions = list(range(nA))
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = 0.01
+        self.epsilon = 1
+        self.epsilon_decay = 0.99999
+        self.epsilon_min = 0.001
+        self.gamma = 1
+        print("alpha", self.alpha, "e_decay", self.epsilon_decay, "e_min", self.epsilon_min, "gamma", self.gamma)
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +31,15 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+
+        # Follow epsilon-greedy policy
+        greedy_choice = np.argmax(self.Q[state])
+        random_choice = np.random.choice(self.actions)
+        epsilon_greedy_choice = np.random.choice(
+            [greedy_choice, random_choice], 
+            p = [1-self.epsilon, self.epsilon]
+        )
+        return epsilon_greedy_choice
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +52,12 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        self.epsilon = max(self.epsilon * self.epsilon_decay, self.epsilon_min)
+
+        # Calculate expected return
+        next_G = 0
+        if not done:
+            next_G = self.epsilon * sum([self.Q[next_state][action] for action in self.actions]) / self.nA + (1 - self.epsilon) * max(self.Q[next_state])
+        
+        # Update Q
+        self.Q[state][action] += self.alpha * ((reward + self.gamma * next_G) - self.Q[state][action])
--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -5,4 +5,4 @@ import numpy as np
 
 env = gym.make('Taxi-v3')
 agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+avg_rewards, best_avg_reward = interact(env, agent, num_episodes=50000)
